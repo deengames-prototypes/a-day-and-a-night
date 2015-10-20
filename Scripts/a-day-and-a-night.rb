a2950cd20a39e59262f5a2b0f37263c24b92aad9
Ashiq A <ashes999@yahoo.com>
2015-10-20 17:46:06 -0400

    Added guage, night/day cycle, 10 points for feeding homeless guy

--- Scripts/a-day-and-a-night.rb
+++ Scripts/a-day-and-a-night.rb
@@ -1,3 +1,15 @@
 #!/env/rb
 API_ROOT = 'Scripts/vx-ace-api'
 require 'Scripts/vx-ace-api/vx_ace_api'
+
+require 'Scripts/mods/advanced_game_time'
+require 'Scripts/mods/euphoria_custom_gauge'  
+
+module ADAAN
+  POINTS = {:fed_homeless => 10}
+  
+  def self.add_points(key)
+    points = POINTS[key]
+    $game_actors[1].add_gauge(points)
+  end
+end
