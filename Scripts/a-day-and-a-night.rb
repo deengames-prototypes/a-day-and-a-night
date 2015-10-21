6ad858774cc203602f170fd72f3ce7967a391c0f
Ashiq A <ashes999@yahoo.com>
2015-10-21 17:47:05 -0400

    Added game-over countdown and global execution.

--- Scripts/a-day-and-a-night.rb
+++ Scripts/a-day-and-a-night.rb
@@ -5,11 +5,15 @@ require 'Scripts/vx-ace-api/vx_ace_api'
 require 'Scripts/mods/advanced_game_time'
 require 'Scripts/utils/custom_save_system'
 require 'Scripts/utils/points_system'  
+require 'Scripts/mods/global_common_events'
 
 module ADAAN
   POINTS = {
     :fed_homeless => 5, :broke_statue => 25
   }
+  
+  # Game ends automagically at 8pm
+  GAME_OVER = {:hour => 20, :minute => 0}
   
   def self.add_points(key)
     points = POINTS[key]
@@ -27,5 +31,13 @@ module ADAAN
 
     RPG::SE.new('Knock', 100, 80 + rand(40)).play
     PointsSystem.add_points('Chopped down tree', -1)
-  end  
+  end
+  
+  def self.countdown_to_game_over
+    if GameTime.hour? == GAME_OVER[:hour] && GameTime.min? == GAME_OVER[:minute]
+      i = Game_Interpreter::instance
+      i.show_message '\N[1] \{UGH!\|They got me!'
+      i.game_over
+    end
+  end
 end
