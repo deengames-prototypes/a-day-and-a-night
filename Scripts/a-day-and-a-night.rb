32351a332870fb7282759c017b19bd44c9f5d50a
Ashiq A <ashes999@yahoo.com>
2015-10-23 17:29:05 -0400

    Added ore mining; refactored random-sound functionality; small map changes

--- Scripts/a-day-and-a-night.rb
+++ Scripts/a-day-and-a-night.rb
@@ -26,25 +26,34 @@ module ADAAN
     PointsSystem.add_points(key, points)
   end
   
+  def self.mine_ore
+    repeat_n_times(3, 3, 'pickaxe')
+  end
+  
   def self.chop_tree
-    # 3-5 knocks, with delay, different pitch
-    # 2-4 random, then one at the end    
-    count = 2 + rand(3)
-    count.times do |i|
-      RPG::SE.new('Knock', 100, 80 + rand(40)).play
-      Game_Interpreter::instance.wait(25)
-    end
-
-    RPG::SE.new('Knock', 100, 80 + rand(40)).play
+    repeat_n_times(3, 6, 'Knock')
     PointsSystem.add_points('Chopped down tree', -1)
   end
   
   def self.countdown_to_game_over
-    if GameTime.hour? == GAME_OVER[:hour] && GameTime.min? == GAME_OVER[:minute]
+    if GameTime.hour? == GAME_OVER[:hour] && GameTime.min? >= GAME_OVER[:minute]
       i = Game_Interpreter::instance
       i.show_message '\N[1] \{UGH!\|They got me!'
       i.show_message "Final score:\n\\{\\{#{PointsSystem.total_points} points!"
       i.game_over
+    end
+  end
+  
+  private
+  
+  # pitch variance of 20 => 80-120
+  # [min_times, max_times)
+  # Repeat this sound min to max times, with random pitch variance
+  def self.repeat_n_times(min_times, max_times, sound, pitch_variance = 20, delay = 25)
+    count = min_times + rand(max_times - min_times).to_i
+    count.times do |i|
+      RPG::SE.new(sound, 100, 100 - pitch_variance + rand(pitch_variance * 2).to_i).play
+      Game_Interpreter::instance.wait(delay)
     end
   end
 end
